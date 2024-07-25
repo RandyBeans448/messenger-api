7a9344c5a7ec6715224523370a3bdb808902de04
Dominic Bishop <dominicbishop@Dominics-MacBook-Pro.local>
2024-07-25 08:31:00 +0100

    25/07/2024 - gateway added to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { AppController } from './app.controller';
 import { JsonBodyMiddleware } from './@utils/middleware/json-body.middleware';
 import { RawBodyMiddleware } from './@utils/middleware/raw-body.middleware';
 import { FriendRequestModule } from './@app-modules/friend-request/friend-request.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [
@@ -31,6 +32,7 @@ import { FriendRequestModule } from './@app-modules/friend-request/friend-reques
     MessageModule,
   ],
   controllers: [AppController],
+  providers: [ChatGateway],
 })
 export class AppModule {
   public configure(consumer: MiddlewareConsumer): void {
