fe298a96569d267f02851f13a89cbc7d8f52a60c
Dominic Bishop <dominicbishop@Dominics-MacBook-Pro.local>
2024-09-17 08:31:12 +0100

    17/09/2024

--- src/@socket/gateway/socket.gateway.ts
+++ src/@socket/gateway/socket.gateway.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Logger, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
+    ConnectedSocket,
     MessageBody,
     OnGatewayConnection,
     OnGatewayDisconnect,
@@ -30,6 +31,7 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
     private _conversation: Conversation;
     private _conversationFriendIds: any[];
+    private publicKeys: { [socketId: string]: string } = {};
 
     constructor(
         private _conversationService: ConversationService,
@@ -42,12 +44,13 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         this.logger.log('Initialized');
     }
 
+    @SubscribeMessage('join')
     public async handleConnection(
         client: Socket,
         conversationId: string,
     ): Promise<void> {
         this.logger.log(`Client connected: ${client.id}`);
- 
+
         this._conversation = await this._conversationService.getConversationById(conversationId);
         this._conversationFriendIds = this._conversation.friend.map((friend) => friend.friend);
 
@@ -57,7 +60,10 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         }
     }
 
-    public handleDisconnect(client: Socket): void {
+    public handleDisconnect(
+        client: Socket,
+    ): void {
+        delete this.publicKeys[client.id];
         this.logger.log(`Client disconnected: ${client.id}`);
     }
 
@@ -71,13 +77,25 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
             conversation: this._conversation,
             sender: payload.senderId === this._conversationFriendIds[0].id ? this._conversationFriendIds[0] : this._conversationFriendIds[1],
             createdAt: payload.createdAt,
-            updatedAt: payload. updatedAt,
+            updatedAt: payload.updatedAt,
         };
 
         await this._messageService.createMessage(
             newMessage,
         );
-
+        console.log(payload)
         this.io.emit('message', payload);
+    }
+
+    // Handle public key exchange
+    @SubscribeMessage('send_public_key')
+    public handlePublicKey(
+        client: Socket,
+        payload: { publicKey: string },
+    ): void {
+        console.log(`Public key received from ${client.id}: ${payload.publicKey}`);
+        this.publicKeys[client.id] = payload.publicKey;
+        console.log('publicKeys ----------->', this.publicKeys, '<----------- publicKeys');
+        this.io.emit('receive_public_key', { publicKey: payload.publicKey });
     }
 }
