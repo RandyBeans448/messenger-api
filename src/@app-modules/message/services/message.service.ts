337a62e7e7d23f30d64f9ad2b3c443379e523768
Dominic Bishop <dominicbishop@Dominics-MacBook-Pro.local>
2025-01-19 17:59:44 +0000

    19/01/2025 - Cover my docker-compose

--- src/@app-modules/message/services/message.service.ts
+++ src/@app-modules/message/services/message.service.ts
@@ -6,28 +6,28 @@ import { MessageNamespace } from '../interfaces/message.interface';
 
 @Injectable()
 export class MessageService {
-    // constructor(
-    //     @InjectRepository(Message)
-    //     private readonly messageRepository: Repository<Message>,
-    // ) { }
+    constructor(
+        @InjectRepository(Message)
+        private readonly messageRepository: Repository<Message>,
+    ) { }
 
-    // public async createMessage(
-    //     payload: MessageNamespace.NewMessageInterface,
-    // ): Promise<Message> {
+    public async createMessage(
+        payload: MessageNamespace.NewMessageInterface,
+    ): Promise<Message> {
 
-    //     const newMessage: Message = new Message();
-    //     newMessage.message = payload.message;
-    //     newMessage.conversation = payload.conversation;
-    //     newMessage.sender = payload.sender;
+        const newMessage: Message = new Message();
+        newMessage.message = payload.message;
+        newMessage.conversation = payload.conversation;
+        newMessage.sender = payload.sender;
 
-    //     try {
-    //         return await this.messageRepository.save(newMessage);
-    //     } catch (error: any) {
-    //         console.error('Error creating message:', error);
-    //         throw new HttpException(
-    //             'Error creating message',
-    //             HttpStatus.INTERNAL_SERVER_ERROR,
-    //         );
-    //     }
-    // }
+        try {
+            return await this.messageRepository.save(newMessage);
+        } catch (error: any) {
+            console.error('Error creating message:', error);
+            throw new HttpException(
+                'Error creating message',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
 }
